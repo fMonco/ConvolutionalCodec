d036111594986ef65bab1407ceca65ab1278b48f
Alexey Mason <orbweaver@mail.ru>
2022-04-07 02:49:18 +0300

    Fixes

--- tags.py
+++ tags.py
@@ -13,27 +13,27 @@ class MyWindow:
     def __init__(self, win):
         self.lbl1=Label(win, text='Enter text')
         self.lbl2=Label(win, text='Enter adders')
-        self.lbl3=Label(win, text='Encoded')
-        self.t1=Entry(bd=3)
+        self.lbl3=Label(win, text='Result')
+        self.t1=Entry()
         self.t2=Entry()
-        self.t3=Entry()
+        self.t3=Text()
         self.btn1 = Button(win, text='Encode')
         self.btn2=Button(win, text='Decode')
-        self.lbl1.place(x=100, y=50)
-        self.t1.place(x=200, y=50)
-        self.lbl2.place(x=100, y=100)
-        self.t2.place(x=200, y=100)
+        self.lbl1.place(x=10, y=10)
+        self.t1.place(x=100, y=10)
+        self.lbl2.place(x=10, y=60)
+        self.t2.place(x=100, y=60)
         self.b1=Button(win, text='Encode', command=self.encode)
         self.b2=Button(win, text='Decode')
         self.b2.bind('<Button-1>', self.decode)
-        self.b1.place(x=100, y=150)
-        self.b2.place(x=200, y=150)
-        self.lbl3.place(x=100, y=200)
-        self.t3.place(x=200, y=200)
-
+        self.b1.place(x=60, y=110)
+        self.b2.place(x=130, y=110)
+        self.lbl3.place(x=10, y=160)
+        self.t3.place(x=100, y=160, width=125, height=70)
 
     def encode(self):
-        self.t3.delete(0, 'end')
+        self.t3.delete(1.0, END)
+        #text = self.t1.get()
         text = text_to_bits(self.t1.get()) 
         nonarray = self.t2.get()
         array = nonarray.split(" ")
@@ -68,7 +68,8 @@ class MyWindow:
 
 
     def decode(self, event):
-        self.t3.delete(0, 'end')
+        self.t3.delete(1.0, END)
+        text = self.t1.get()
         self.t3.insert(END, text)
  
 
@@ -77,5 +78,5 @@ class MyWindow:
 window=Tk()
 mywin=MyWindow(window)
 window.title('ConvolutionalCoder')
-window.geometry("400x300+10+10")
+window.geometry("250x250")
 window.mainloop()
